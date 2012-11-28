46bb4d3e51791f44d08288b83b53981d5af39773
Nuno Hespanhol <nuno.hespanhol@gmail.com>
2012-11-28 13:55:43 +0000

    - working for 110 containers (first version, still needs improving)

--- script.js
+++ script.js
@@ -2,21 +2,53 @@ var map;
 var directionDisplay;
 var directionsService;
 
+var totalDistance = 0;
+
 // TODO
 var centerPoint = [ 55.862743, 9.836143 ];
 
-var points = [];
 var markers = [];
 
 var infoWindow = null;
 
 var DEFAULT_ZOOM = 12;
 
+function requestDirections(start, end) {
+    directionsService.route({
+        origin: start,
+        destination: end,
+        travelMode: google.maps.DirectionsTravelMode.DRIVING
+    }, function(result, status) {
+        if (status == google.maps.DirectionsStatus.OK)
+            renderDirections(result);
+        else if (status == google.maps.DirectionsStatus.OVER_QUERY_LIMIT) {
+            setTimeout(function() { requestDirections(start, end); }, 1000);
+            console.log("waiting one second, before next request");
+        }
+        else {
+            console.log("some other unknown error");
+        }
+    });
+}
+
+
+function renderDirections(result) {
+    var directionsRenderer = new google.maps.DirectionsRenderer({ 
+        supressMarkers: true, 
+        supressInfoWindows: true,
+        map: map,
+        directions: result
+    });
+    // directionsRenderer.setMap(map);
+    // directionsRenderer.setDirections(result);
+}
+
 function initialize() {
 
     // initializing google maps stuff
     directionsService = new google.maps.DirectionsService();
-    directionsDisplay = new google.maps.DirectionsRenderer();
+    //directionsDisplay = new google.maps.DirectionsRenderer({ supressMarkers: true, supressInfoWindows: true });
+
 
     var mapOptions = {
         zoom: DEFAULT_ZOOM,
@@ -26,7 +58,7 @@ function initialize() {
 
     map = new google.maps.Map(document.getElementById('map_canvas'), mapOptions);
 
-    directionsDisplay.setMap(map);
+    //directionsDisplay.setMap(map);
 
     infoWindow = new google.maps.InfoWindow();
     //infoWindow.setContent("loading...");
@@ -39,13 +71,14 @@ function clearMap() {
         markers[i].setMap(null);
 
     // clear route
-    directionsDisplay.setMap(null);
+    //directionsDisplay.setMap(null);
 }
 
 function show(url) {
 
     clearMap();
-    points = new Array();
+
+    markers = new Array();
 
     $.get(url, function(data) {
         
@@ -86,35 +119,24 @@ function showFull() {
     show("php/pathfinder.php");
 }
 
+function showRoute2(){
+    requestDirections("Huntsville, AL", "Boston, MA");
+    requestDirections("California, CA", "Stanford, CA");
+}
+
 function showRoute() {
 
     clearMap();
 
-    directionsDisplay.setMap(map);
-
-    var start = markers[0].position;
-
-    var waypts = new Array();
-
-    for (i = 1; i < points.length; i++) {
-        waypts[i-1] = { location: points[i], stopover: true };
+    for (i = 0; i < markers.length-1; i++) {
+        var marker1 = markers[i];
+        var marker2 = markers[i+1];
+        requestDirections(marker1.position, marker2.position);
+        // window.setTimeout(function() { alert("foo"); }, 5000);
+        // console.log("after requesting " + i);
+        //window.clearTimeout(handle);
     }
-
-    var request = {
-        origin: start,
-        destination: start,
-        waypoints: waypts,
-        optimizeWaypoints: true,
-        unitSystem: google.maps.UnitSystem.METRIC,
-        travelMode: google.maps.DirectionsTravelMode.DRIVING
-    };
-
-    directionsService.route(request, function(response, status) {
-        if (status == google.maps.DirectionsStatus.OK) {
-            directionsDisplay.setDirections(response);
-            showSteps(response);
-        }
-    });
+    //requestDirections(markers[markers.length-1], markers[0]);
 }
 
 function clean() {
@@ -128,10 +150,16 @@ function showSteps(directionResult) {
     directionsPanel.innerHTML = "";
     for (var l = 0; l < directionResult.routes[0].legs.length; l++) {
         var myRoute = directionResult.routes[0].legs[l];
-        directionsPanel.innerHTML += "<h3>" + myRoute.start_address + " - " + myRoute.end_address + "</h3>";
+        var dist = myRoute.distance.value / 1000.0;
+        totalDistance += dist;
+        directionsPanel.innerHTML += "<h3>" + myRoute.distance.text + " " + myRoute.start_address + " - " + myRoute.end_address + "</h3>";
         for (var i = 0; i < myRoute.steps.length; i++) {          
             var inst = myRoute.steps[i].instructions;
             directionsPanel.innerHTML += "<p>" + (i+1) + ". " + inst + "</p>";
         }
     }
+
+    directionsPanel.innerHTML += "Total <h1>" + totalDistance.toFixed(1) + " km </h1>"
+    
 }
+
